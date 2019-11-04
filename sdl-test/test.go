31d8554bfccbfae20d12e188e0e437e379bd4945
liangdas <1587790525@qq.com>
2019-11-04 10:55:58 +0800

    =

--- sdl-test/test.go
+++ sdl-test/test.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/0xe2-0x9a-0x9b/Go-SDL/mixer"
-	"github.com/0xe2-0x9a-0x9b/Go-SDL/sdl"
-	"github.com/0xe2-0x9a-0x9b/Go-SDL/ttf"
+	"github.com/scottferg/Go-SDL/mixer"
+	"github.com/scottferg/Go-SDL/sdl"
+	"github.com/scottferg/Go-SDL/ttf"
 	"log"
 	"math"
 	"os"
